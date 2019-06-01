05f38c55ee2e081c0e8219f94f93d94e7d4cd7a0
1nhee <21800025@handong.edu>
2019-06-01 23:07:27 +0900

    do I have?

--- src/main/java/edu/handong/csee/java/examples/Runner.java
+++ src/main/java/edu/handong/csee/java/examples/Runner.java
@@ -9,11 +9,15 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 
+import additionalHw.LsCLI;
+
 public class Runner {
 	
-	String path;
-	boolean fullPath;
-	boolean verbose;
+	String input_path;
+	boolean list;
+	boolean absolute;
+	boolean lastModified;
+	boolean size;
 	boolean help;
 
 	public static void main(String[] args) {
@@ -23,7 +27,7 @@ public class Runner {
 
 	}
 
-	private void run(String[] args) {
+	void run(String[] args) {
 		Options options = createOptions();
 		
 		if(parseOptions(options, args)){
@@ -31,27 +35,62 @@ public class Runner {
 				printHelp(options);
 				return;
 			}
+		}
 			
 			// path is required (necessary) data so no need to have a branch.
-			System.out.println("You provided \"" + path + "\" as the value of the option p");
+			System.out.println("This is result of 'p' option");
+			System.out.println("You put this path: " + input_path);
 			
 			// TODO show the number of files in the path
 			
-			File file = new File(path);
-			System.out.println(file.getAbsolutePath());
-			System.out.println(file.getName());
-			System.out.println(file.listFiles().length);
-			if(verbose) {
+			File file = new File(input_path);
+		/*
+		 * System.out.println(file.getAbsolutePath());
+		 * System.out.println(file.getName());
+		 * System.out.println(file.listFiles().length);
+		 */
+			
+			//absolute
+			if(absolute) {
+				System.out.println("This is result of 'a' option");
+				System.out.println("These are absolute pathes in the path");
 				
-				// TODO list all files in the path
 				 for(int i = 0; i < file.listFiles().length; i++) {
 					 System.out.println(file.getAbsolutePath());
-				 }
+				 } 
+				 
+				 System.out.println(" ");
+				//list all file name
+			}else if(list) {
+				System.out.println("This is result of 'l' option");
+				System.out.println("This is list of files in the path");
 				
-				System.out.println("Your program is terminated. (This message is shown because you turned on -f option!");
+				for(int i = 0; i < file.listFiles().length; i++) {
+					System.out.println(file.getName());
+				}
+				System.out.println(" ");
+				
+			}else if(lastModified) {
+				System.out.println("This is result of 'm' option");
+				System.out.println("These are the date that the file is modifieded lastly in the path");
+				
+				for(int i = 0; i < file.listFiles().length; i++) {
+					System.out.println(file.lastModified());
+				}
+				
+				System.out.println(" ");
+			}else if(size) {
+				System.out.println("This is result of 's' option");
+				System.out.println("These are the size of files");
+				
+				for(int i = 0; i < file.listFiles().length; i++) {
+					System.out.println(file.length());
+				}
+				System.out.println(" ");
 			}
-		}
-	}
+				
+				System.out.println("Your program is terminated. Your program is done!");
+		} 
 
 	private boolean parseOptions(Options options, String[] args) {
 		CommandLineParser parser = new DefaultParser();
@@ -60,9 +99,11 @@ public class Runner {
 
 			CommandLine cmd = parser.parse(options, args);
 
-			path = cmd.getOptionValue("p");
-			fullPath = cmd.hasOption("f");
-			verbose = cmd.hasOption("v");
+			input_path = cmd.getOptionValue("i");
+			list = cmd.hasOption("l");
+			absolute = cmd.hasOption("a");
+			lastModified = cmd.hasOption("m");
+			size = cmd.hasOption("s");
 			help = cmd.hasOption("h");
 
 		} catch (Exception e) {
@@ -77,28 +118,38 @@ public class Runner {
 	private Options createOptions() {
 		Options options = new Options();
 		
-		// add options by using OptionBuilder
-				options.addOption(Option.builder("p").longOpt("path")
+		// input
+				options.addOption(Option.builder("i").longOpt("Input_path")
 						.desc("Set a path of a directory or a file to display")
 						.hasArg()
 						.argName("Path name to display")
 						.required()
 						.build());
 		
-		// add options by using OptionBuilder
-		options.addOption(Option.builder("f").longOpt("fullpath")
-						.desc("Set a path of a directory or a file to display")
-						.argName("Full path name to display")
+		// absolute path
+		options.addOption(Option.builder("a").longOpt("absolute_path")
+						.desc("display an abslute path of a directory or a file")
+						.argName("absolute path name")
 						.build());
 
 		
-		// add options by using OptionBuilder
-		options.addOption(Option.builder("v").longOpt("verbose")
-						.desc("Display detailed messages!")
-						//.hasArg()     // this option is intended not to have an option value but just an option
-						.argName("verbose option")
-						//.required() // this is an optional option. So disabled required().
+		// list all
+		options.addOption(Option.builder("l").longOpt("list")
+						.desc("Display list of files in the directory")
+						.argName("list")
 						.build());
+		
+		// last modified
+		options.addOption(Option.builder("m").longOpt("last_modified")
+				.desc("Display the date the file is modified lastly")
+				.argName("last modified")
+				.build());
+		
+		// last modified
+		options.addOption(Option.builder("s").longOpt("size")
+				.desc("Display the size of files")
+				.argName("size")
+				.build());
 				
 		// add options by using OptionBuilder
 		options.addOption(Option.builder("h").longOpt("help")
@@ -111,8 +162,8 @@ public class Runner {
 	private void printHelp(Options options) {
 		// automatically generate the help statement
 		HelpFormatter formatter = new HelpFormatter();
-		String header = "CLI test program";
-		String footer ="\nPlease report issues at https://github.com/lifove/CLIExample/issues";
+		String header = "ls CLI";
+		String footer ="";
 		formatter.printHelp("CLIExample", header, options, footer, true);
 	}
 
